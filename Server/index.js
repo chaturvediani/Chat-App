42bb7d1728c640cf63223207490b8705a2edb6e7
Animesh <animeshc123@gmail.com>
2022-06-17 00:13:21 +0530

    second commit

---
+++ Server/index.js
@@ -0,0 +1,25 @@
+const express=require('express')
+const app=express()
+const http=require('http')
+const server=http.createServer(app);
+const io = require('socket.io')(server, {
+    cors: {
+      origin: '*',
+    }
+  });
+const users={}
+io.on('connection', (socket)=>{
+    console.log('connected!')
+    socket.on('new-user-joined',name=>{
+        console.log('new user:', name);
+      users[socket.id]=name;
+      socket.broadcast.emit('user-joined',name)
+    });
+    socket.on('send',(msg)=>{
+        socket.broadcast.emit('recieve',{message:msg,name:users[socket.id]});
+    });
+
+})
+server.listen(3000,()=>{
+    console.log('listening on :3000');
+});
